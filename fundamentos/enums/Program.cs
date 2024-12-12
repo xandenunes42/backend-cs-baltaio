5f34eaa34256488b67ded33fad4f18a02f52165a
Alexandre Nunes <alexandre.oli.nunes@gmail.com>
2024-12-12 04:13:09 -0300

    enums

---
+++ fundamentos/enums/Program.cs
@@ -0,0 +1,25 @@
+﻿
+var pessoa1 = new Pessoa("Alexandre", EEstadoCivil.Solteiro);
+
+Console.WriteLine($"{pessoa1.Nome} - {pessoa1.EstadoCivil}.");
+
+//Anatomia
+enum EEstadoCivil
+{   
+    Solteiro = 1,
+    Casado = 2,
+    Divorciado = 3
+}
+
+struct Pessoa
+{
+    public Pessoa(string nome, EEstadoCivil eEstadoCivil)
+    {
+        Nome = nome;
+        EstadoCivil = eEstadoCivil;
+    }
+
+    public string Nome;
+    public EEstadoCivil EstadoCivil;
+
+}
