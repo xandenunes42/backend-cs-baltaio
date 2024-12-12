0bcf3ba4755cfea6278eb3f37ac87ea5a4967dd4
Alexandre Nunes <alexandre.oli.nunes@gmail.com>
2024-12-11 23:39:48 -0300

    valueTypes-referenceTypes

---
+++ fundamentos/valueTypes-referenceTypes/Program.cs
@@ -0,0 +1,29 @@
+﻿//Value Types
+
+int x = 1;
+int y = 2;
+
+y = x; // y recebe uma cópia de x
+
+Console.WriteLine(x); // 1
+Console.WriteLine(y); // 1 - cópia de x 
+
+x = 10;
+
+Console.WriteLine(x); // 10
+Console.WriteLine(y); // somente x eh alterado
+
+//Reference Types 
+
+var array = new string[2];
+array[0] = "Item 1";
+
+Console.WriteLine(array[0]);
+
+var refArray = array; // Não cria uma cópia
+
+Console.WriteLine(refArray[0]);
+
+refArray[0] = "Item Alterado";
+
+Console.WriteLine(array[0]); // Valor alterado, pois a referencia aponta pro mesmo lugar
