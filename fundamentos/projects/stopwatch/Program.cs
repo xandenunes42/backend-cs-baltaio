066cb2eebf393ecdd79c9fd2f3ae7f15e6fb0377
Alexandre Nunes <alexandre.oli.nunes@gmail.com>
2024-12-16 11:42:42 -0300

    project stopwatch

---
+++ fundamentos/projects/stopwatch/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+
+string input = "10s";
+
+char letter = char.Parse(input.Substring(input.Length - 1, 1));
+//Captura o ultimo elemento da string
+
+int time = int.Parse(input.Substring(0, input.Length - 1));
+//Captura todos elementos da string com excecao do ultimo
+
+Console.Write("Iniciando cronometro");
+Thread.Sleep(1000);
+Console.Write(".");
+Thread.Sleep(1000);
+Console.Write(".");
+Thread.Sleep(1000);
+Console.Write(".");
+
+Console.Clear();
+
+while(time >= 0)
+{
+    Console.Clear();
+    Console.WriteLine(time);
+    Thread.Sleep(1000);
+    time--;
+}
+
+Console.Clear();
+Console.WriteLine("Fim do cronometro !!!");
+
