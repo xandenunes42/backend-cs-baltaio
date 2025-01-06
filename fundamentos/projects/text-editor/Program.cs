095753866b0ef66c96606e6b17d56af2e8569795
Alexandre Nunes <alexandre.oli.nunes@gmail.com>
2025-01-06 20:01:54 -0300

    text editor menu

---
+++ fundamentos/projects/text-editor/Program.cs
@@ -0,0 +1,27 @@
+﻿static void Menu()
+{
+    Console.WriteLine("Selecione uma opcao:\n1 - Criar novo arquivo\n2 - Editar arquivo existente\n0 - Sair");
+
+    short option = short.Parse(Console.ReadLine());
+
+    switch (option)
+    {
+        case 0: System.Environment.Exit(0); break;
+        case 1: OpenFile(); break;
+        case 2: EditFile(); break;
+    }
+}
+
+static void OpenFile()
+{
+
+}
+
+static void EditFile()
+{
+
+}
+
+Menu();
+
+
