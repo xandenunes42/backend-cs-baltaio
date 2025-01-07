6deed22d32a45e707210559e181035d79917a011
Alexandre Nunes <alexandre.oli.nunes@gmail.com>
2025-01-07 13:29:38 -0300

    Guid-Globally-Unique-Identifier

---
+++ fundamentos/guids/Program.cs
@@ -0,0 +1,8 @@
+﻿//Guid -> Globally Unique Identifier
+//Geralmente utilizado para gerar um valor random
+
+var id = Guid.NewGuid();
+//capaz de gerar um identificador unico nesse formato -> 762a8e50-ed9e-45d5-a349-69403201f4dd
+
+id.ToString();
+Console.WriteLine(id);
