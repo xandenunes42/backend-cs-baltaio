4573ea9919f1ad895e2b4101ed145ee7022bee78
Alexandre Nunes <alexandre.oli.nunes@gmail.com>
2025-01-08 14:45:27 -0300

    project-html-editor

---
+++ fundamentos/projects/editor-html/Menu.cs
@@ -0,0 +1,66 @@
+using System.Security.Principal;
+
+namespace MenuScreen;
+
+public static class Menu
+{
+    public static void Show()
+    {
+        DrawScreen();
+        TextMenu();
+    }
+
+    public static void DrawScreen()
+    {
+        DrawLines();
+        DrawCollumns();
+        DrawLines();
+    }
+
+    static void DrawLines()
+    {
+        Console.Write("\n");
+        Console.Write("+");
+
+        for(int i = 0; i <= 30; i++)
+        {
+            Console.Write("-");
+        }
+
+        Console.Write("+");
+    }
+
+    static void DrawCollumns()
+    {
+        for(int i = 0; i <= 10; i++)
+        {
+            Console.Write("\n");
+            Console.Write("|");
+
+            for(int j = 0; j <= 30; j++)
+            {
+                Console.Write(" ");
+            }
+            
+            Console.Write("|");
+        }
+    }
+
+    static void TextMenu()
+    {
+        Console.SetCursorPosition(2, 1);
+        Console.Write("HTML EDITOR");
+        Console.SetCursorPosition(2, 3);
+        Console.Write("SELECIONE UMA OPCAO: ");
+        Console.SetCursorPosition(2, 5);
+        Console.Write("1 - NOVO ARQUIVO");
+        Console.SetCursorPosition(2, 7);
+        Console.Write("2 - ABRIR");
+        Console.SetCursorPosition(2, 9);
+        Console.Write("0 - SAIR");
+        
+        Console.SetCursorPosition(2, 11);
+        Console.Write("OPCAO: ");
+        short op = short.Parse(Console.ReadLine());
+    }
+}
