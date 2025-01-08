623364b58d478418856bd2c923effe658ea6c067
Alexandre Nunes <alexandre.oli.nunes@gmail.com>
2025-01-08 13:55:09 -0300

    string-indexOf

---
+++ fundamentos/string-index/Program.cs
@@ -0,0 +1,7 @@
+﻿var text = "texto de teste";
+
+Console.WriteLine(text.IndexOf("t"));
+// Retorna 6, posicao de "d"
+
+Console.WriteLine(text.LastIndexOf("t"));
+// Retorna o ultimo indice contendo "t" -> 12
