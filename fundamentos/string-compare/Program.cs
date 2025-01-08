3be88a40e7869f1712f946434b57276419039c57
Alexandre Nunes <alexandre.oli.nunes@gmail.com>
2025-01-08 13:26:58 -0300

    string-compare

---
+++ fundamentos/string-compare/Program.cs
@@ -0,0 +1,11 @@
+﻿//comparando string utilizando CompareTo
+
+var text = "teste";
+
+Console.WriteLine(text.CompareTo("teste")); //retorna 0, pois sao iguais
+Console.WriteLine(text.CompareTo("teste1")); //retorna -1, pois sao diferentes
+
+//utilizando Contains
+
+Console.WriteLine(text.Contains("teste")); //retorna true
+
